e0b42cd81f51e23625b5caa37100107b05f36c32
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-06-15 11:14:51 -0500

    Add Development branch

--- the-garage/src/App.jsx
+++ the-garage/src/App.jsx
@@ -1,10 +1,12 @@
-import "./App.css";
-import ItemList from "./layouts/ItemList";
-import Login from "./layouts/login";
+import './App.css';
+import { Home } from './layouts/Home';
+import ItemList from './layouts/ItemList';
+import Login from './layouts/login';
 
 function App() {
   return (
     <>
+      <Home />
       <Login />
       <ItemList />
     </>
--- the-garage/src/layouts/ItemList.jsx
+++ the-garage/src/layouts/ItemList.jsx
@@ -1,8 +1,8 @@
-import Item from "./Item";
+import Item from './Item';
 
 function ItemList() {
   return (
-    <container className="d-flex gap-2 flex-wrap justify-content-sm-around">
+    <container className='d-flex gap-2 flex-wrap justify-content-sm-around'>
       <Item />
       <Item />
       <Item />
