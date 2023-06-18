786da3db1327d4dd562ef82b58e99396b87ac0fa
welinton <wesgdeveloper@gmail.com>
2023-06-18 12:14:31 -0500

    refactor: Create component Paginator

--- the-garage/src/layouts/ItemList.jsx
+++ the-garage/src/layouts/ItemList.jsx
@@ -2,6 +2,7 @@ import { Col, Container, Pagination, Row } from "react-bootstrap";
 import Item from "./Item";
 import Controls from "./Controls";
 import Filter from "./Filter";
+import Paginator from "./Paginator";
 
 function ItemList() {
   return (
@@ -33,25 +34,7 @@ function ItemList() {
             <Item />
             <Item />
           </div>
-          <div className="d-flex justify-content-center pt-3">
-            <Pagination>
-              <Pagination.First />
-              <Pagination.Prev />
-              <Pagination.Item>{1}</Pagination.Item>
-              <Pagination.Ellipsis />
-
-              <Pagination.Item>{10}</Pagination.Item>
-              <Pagination.Item>{11}</Pagination.Item>
-              <Pagination.Item active>{12}</Pagination.Item>
-              <Pagination.Item>{13}</Pagination.Item>
-              <Pagination.Item disabled>{14}</Pagination.Item>
-
-              <Pagination.Ellipsis />
-              <Pagination.Item>{20}</Pagination.Item>
-              <Pagination.Next />
-              <Pagination.Last />
-            </Pagination>
-          </div>
+          <Paginator />
         </Col>
       </Row>
     </Container>
