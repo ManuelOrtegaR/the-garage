8320f630299cf3fac31a86e99d7f97e1efdb1eb3
welinton <wesgdeveloper@gmail.com>
2023-06-20 09:52:24 -0500

    add icon-boostrap

--- the-garage/src/main.jsx
+++ the-garage/src/main.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from '@emotion/react';
-import theme from './theme';
-import App from './App.jsx';
-import './theme/index.css';
-import './theme/custom.scss';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./theme";
+import App from "./App.jsx";
+import "./theme/index.css";
+import "./theme/custom.scss";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 );
