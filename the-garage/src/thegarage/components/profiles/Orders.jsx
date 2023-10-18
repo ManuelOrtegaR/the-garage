45768962fc2cc2c4880238b81956fac4bc6dc163
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-10-18 10:04:16 -0500

    Add filters and finish requests methods

--- the-garage/src/thegarage/components/profiles/Orders.jsx
+++ the-garage/src/thegarage/components/profiles/Orders.jsx
@@ -5,7 +5,7 @@ import {
   ShowOrder,
   StatusStyle,
 } from './StylesComponentsProfiles';
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import { PaginationProfiles } from './PaginationProfiles';
 import { useNavigate } from 'react-router-dom';
 import { useOrders } from '../../../domain/useOrders';
@@ -14,13 +14,44 @@ import { AuthContext } from '../../../auth/context/AuthContext';
 export const Orders = () => {
   const navigate = useNavigate();
   const { user } = useContext(AuthContext);
-  const { data } = useOrders();
+  const { data: responseData } = useOrders();
+  const [data, setData] = useState([]);
+  const initialData = useRef(null);
+
+  useEffect(() => {
+    if (responseData) {
+      setData(responseData);
+      initialData.current = responseData;
+    }
+  }, [responseData]);
+
   const [productsBypage, setproductsByPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
   const totalOrders = data.length;
-
   const lastIndex = currentPage * productsBypage;
   const firstIndex = lastIndex - productsBypage;
+
+  const [filtroSelected, setFiltroSelected] = useState('Todo');
+
+  const handleFiltro = (e) => {
+    const valorSeleccionado = e.target.value;
+    setFiltroSelected(valorSeleccionado);
+
+    const filtered = initialData.current.filter((order) => {
+      if (valorSeleccionado === 'Todo') {
+        return order;
+      } else if (valorSeleccionado === '1') {
+        return order.estados[order.estados.length - 1].estado === 'Pagada';
+      } else if (valorSeleccionado === '2') {
+        return order.estados[order.estados.length - 1].estado === 'Enviada';
+      } else if (valorSeleccionado === '3') {
+        return order.estados[order.estados.length - 1].estado === 'Entregada';
+      } else if (valorSeleccionado === '4') {
+        return order.estados[order.estados.length - 1].estado === 'Cancelada';
+      }
+    });
+    setData(filtered);
+  };
 
   const orderDetails = (order, index) => {
     navigate(`${order.id}`, { state: data[index] });
@@ -33,66 +64,78 @@ export const Orders = () => {
           <span className="fs-6 fw-bold">Ordenes</span>
           <div className="d-flex w-25  align-items-center">
             <span className="w-50">Filtrar por: </span>
-            <select className="form-select" aria-label="Default select example">
+            <select
+              className="form-select"
+              aria-label="Default select example"
+              value={filtroSelected}
+              onChange={handleFiltro}
+            >
               <option defaultValue="Todo">Todo</option>
-              <option value="1">Estado</option>
-              <option value="2">Fecha</option>
-              <option value="3">Tienda</option>
+              <option value="1">Pagadas</option>
+              <option value="2">Enviadas</option>
+              <option value="3">Entregadas</option>
+              <option value="4">Canceladas</option>
             </select>
           </div>
         </div>
         <ListGroupStyle>
-          {data
-            .map((order, index) => (
-              <div key={index}>
-                <ItemStyle key={order.id} className="border-bottom">
-                  {user.userclass !== 'Administrador' ? (
-                    <Image
-                      src={
-                        user.userClass === 'Cliente'
-                          ? order.foto_empresa
-                          : order.foto_cliente
-                      }
-                      style={{ height: 65, width: 65 }}
-                    />
-                  ) : (
-                    <>
+          {data.length > 0 ? (
+            data
+              .map((order, index) => (
+                <div key={index}>
+                  <ItemStyle key={order.id} className="border-bottom">
+                    {user.userclass !== 'Administrador' ? (
                       <Image
-                        src={order.foto_cliente}
+                        src={
+                          user.userClass === 'Cliente'
+                            ? order.foto_empresa
+                            : order.foto_cliente
+                        }
                         style={{ height: 65, width: 65 }}
                       />
-                      <Image
-                        src={order.foto_empresa}
-                        style={{ height: 65, width: 65 }}
+                    ) : (
+                      <>
+                        <Image
+                          src={order.foto_cliente}
+                          style={{ height: 65, width: 65 }}
+                        />
+                        <Image
+                          src={order.foto_empresa}
+                          style={{ height: 65, width: 65 }}
+                        />
+                      </>
+                    )}
+                    <span className="col-2">
+                      {order.detalle_orden_productos[0].nombre}
+                    </span>
+                    <span className="col-1">
+                      Articulos: {order.detalle_orden_productos.length}
+                    </span>
+                    <span className="col-2 fw-bold fs-5 ">
+                      {'$' + new Intl.NumberFormat('es-Co').format(order.total)}
+                    </span>
+                    <div className="col-2">
+                      <StatusStyle
+                        className={
+                          order.estados[order.estados.length - 1].estado
+                        }
+                      >
+                        {order.estados[order.estados.length - 1].estado}
+                      </StatusStyle>
+                    </div>
+                    <ShowOrder onClick={() => orderDetails(order, index)}>
+                      <i
+                        className="bi bi-eye-fill"
+                        style={{ width: '20px', height: '20px' }}
                       />
-                    </>
-                  )}
-                  <span className="col-2">
-                    {order.detalle_orden_productos[0].nombre}
-                  </span>
-                  <span className="col-1">
-                    Articulos: {order.detalle_orden_productos.length}
-                  </span>
-                  <span className="col-2 fw-bold fs-5 ">
-                    {'$' + new Intl.NumberFormat('es-Co').format(order.total)}
-                  </span>
-                  <div className="col-2">
-                    <StatusStyle
-                      className={order.estados[order.estados.length - 1].estado}
-                    >
-                      {order.estados[order.estados.length - 1].estado}
-                    </StatusStyle>
-                  </div>
-                  <ShowOrder onClick={() => orderDetails(order, index)}>
-                    <i
-                      className="bi bi-eye-fill"
-                      style={{ width: '20px', height: '20px' }}
-                    />
-                  </ShowOrder>
-                </ItemStyle>
-              </div>
-            ))
-            .slice(firstIndex, lastIndex)}
+                    </ShowOrder>
+                  </ItemStyle>
+                </div>
+              ))
+              .slice(firstIndex, lastIndex)
+          ) : (
+            <div>No hay ordenes</div>
+          )}
         </ListGroupStyle>
         <PaginationProfiles
           byPage={productsBypage}
--- the-garage/src/thegarage/components/profiles/Requests.jsx
+++ the-garage/src/thegarage/components/profiles/Requests.jsx
@@ -1,3 +1,4 @@
+import { format } from 'date-fns';
 import {
   ItemStyle,
   ListGroupStyle,
@@ -7,7 +8,7 @@ import {
 import Image from 'react-bootstrap/Image';
 import Form from 'react-bootstrap/Form';
 import { PaginationProfiles } from './PaginationProfiles';
-import { useState, useContext } from 'react';
+import { useState, useContext, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../auth/context/AuthContext';
 import { useCompanyApprove } from '../../../domain/useCompanyApprove';
@@ -15,14 +16,41 @@ import { useCompanyApprove } from '../../../domain/useCompanyApprove';
 export const Requests = () => {
   const navigate = useNavigate();
   const { user } = useContext(AuthContext);
-  const { data } = useCompanyApprove();
+  const { data: responseData } = useCompanyApprove();
+  const [data, setData] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const initialData = useRef(null);
 
   const [requestBypage, setRequestByPage] = useState(5);
   const [currentPage, setCurrentPage] = useState(1);
   const totalRequests = data.length;
 
+  useEffect(() => {
+    if (responseData) {
+      setData(responseData);
+      initialData.current = responseData;
+    }
+  }, [responseData]);
+
   const lastIndex = currentPage * requestBypage;
   const firstIndex = lastIndex - requestBypage;
+
+  const onSearchChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  useEffect(() => {
+    if (searchValue === '') {
+      setData(initialData.current);
+    } else {
+      const filtered = initialData.current.filter((account) => {
+        return account.empresa.razon_social
+          .toLowerCase()
+          .includes(searchValue.toLowerCase());
+      });
+      setData(filtered);
+    }
+  }, [searchValue]);
 
   return (
     <div className="w-100 m-3">
@@ -34,16 +62,9 @@ export const Requests = () => {
           placeholder="Buscar Por Nombre"
           className="me-2 w-50"
           aria-label="Search"
+          value={searchValue}
+          onChange={onSearchChange}
         />
-        <div className="d-flex text-nowrap align-items-center">
-          <span>Ordenar por: </span>
-          <select className="form-select" aria-label="Default select example">
-            <option selected>Todo</option>
-            <option value="1">Estado</option>
-            <option value="2">Fecha</option>
-            <option value="3">Tienda</option>
-          </select>
-        </div>
       </div>
 
       <ListGroupStyle>
@@ -59,7 +80,9 @@ export const Requests = () => {
                 <span className="col-1">
                   NIT: {company.empresa.numero_documento_empresa}
                 </span>
-                <span className="col-2 fw-bold fs-5 ">fecha de solicitud</span>
+                <span className="col-2">
+                  {format(new Date(company.fecha_creacion), 'dd/MM/yyyy')}
+                </span>
                 <div className="col-2">
                   <StatusStyle className="Pagada">
                     {company.estatus}
