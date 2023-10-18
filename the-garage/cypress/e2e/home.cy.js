6fbbc5b6fbcdcdc7a05740e55ecbc57f5e76107b
Manuel Ortega Ruiz <manuellortegaruiz@gmail.com>
2023-10-18 11:56:51 -0500

    update home and signin e2e tests

--- the-garage/cypress/e2e/home.cy.js
+++ the-garage/cypress/e2e/home.cy.js
@@ -1,15 +1,24 @@
 /* eslint-disable no-undef */
-describe('Home', () => {
+describe('Test Home page', () => {
   it('Charge home page', () => {
     cy.visit('/');
 
     cy.contains('¿Cómo funciona?');
     cy.contains('¿Quiénes somos?');
     cy.get(`[aria-label="login-button"]`).should('be.visible');
-    cy.get('.nav-pills')
-      .children()
-      .first()
-      .should('have.class', 'active')
-      .and('have.text', 'Inicio');
+    cy.get('.nav-pills').children().first().and('have.text', 'Inicio');
+  });
+  it('Charge companies and reviews', () => {
+    cy.visit('/');
+    cy.intercept('GET', '/api/v1/perfil/empresas', {
+      fixture: 'companies.json',
+    });
+    cy.intercept('GET', '/api/v1/valoraciones?orderBy=fecha_creacion', {
+      fixture: 'reviews.json',
+    });
+    cy.contains('Empresas');
+    cy.get(`[data-cy="company-container"]`).children().should('have.length', 2);
+    cy.contains('Comentarios');
+    cy.get(`[data-cy="review-item"]`).children().should('have.length', 2);
   });
 });
--- the-garage/cypress/e2e/signin.cy.js
+++ the-garage/cypress/e2e/signin.cy.js
@@ -4,11 +4,46 @@ describe('Sign In', () => {
     cy.visit('/login');
     cy.contains('Inicio de Sesión');
     cy.get('#formBasicEmail');
-    cy.get('input[name="email"]').type('inquisidor-x@hotmail.com');
-    cy.get('input[name="password"]').type('Nueva123{enter}');
-    cy.contains('Vendedores de cajas');
+    cy.get('input[name="email"]').type('client@example.com');
+    cy.get('input[name="password"]').type('Client123{enter}');
+    cy.intercept('POST', '/api/v1/auth/signin', {
+      fixture: 'signInResponse.json',
+    });
+    cy.contains('Manuel Ortega');
     cy.get('.nav-link').click();
     cy.get(`[aria-label="logout"]`).click();
+    cy.intercept('GET', '/api/v1/perfil/empresas', {
+      fixture: 'companies.json',
+    });
+    cy.intercept('GET', '/api/v1/valoraciones?orderBy=fecha_creacion', {
+      fixture: 'reviews.json',
+    });
     cy.get(`[aria-label="login-button"]`).should('be.visible');
+  });
+
+  it('Sign in with incorrect data', () => {
+    cy.visit('/login');
+    cy.contains('Inicio de Sesión');
+    cy.get('#formBasicEmail');
+    cy.get('input[name="email"]').type('incorrect@example.com');
+    cy.get('input[name="password"]').type('Client123{enter}');
+    cy.intercept('POST', '/api/v1/auth/signin', {
+      statusCode: 403,
+      fixture: 'signInError.json',
+    });
+    cy.contains('Correo o contraseña invalidos');
+  });
+
+  it('Sign in with an bloqued or unnactive account', () => {
+    cy.visit('/login');
+    cy.contains('Inicio de Sesión');
+    cy.get('#formBasicEmail');
+    cy.get('input[name="email"]').type('incorrect@example.com');
+    cy.get('input[name="password"]').type('Client123{enter}');
+    cy.intercept('POST', '/api/v1/auth/signin', {
+      statusCode: 403,
+      fixture: 'signInBloqued.json',
+    });
+    cy.contains('Usuario no activo o bloqueado');
   });
 });
