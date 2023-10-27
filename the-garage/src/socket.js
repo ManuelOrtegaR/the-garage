7e58a8c574fbcced6d87d30edcd7f331255b4dc6
Welinton Suarez <wesgdeveloper@gmail.com>
2023-10-27 14:26:59 -0500

    update link render

--- the-garage/src/socket.js
+++ the-garage/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:3000");
+const socket = io("https://the-garage-api.onrender.com");
 
 socket.on("connect", () => {
   console.log("conectado to the server");
